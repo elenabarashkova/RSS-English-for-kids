46221503b1bad73b54a7469276be819e06ea3c09
elenabarashkova <lena.barashkova23@gmail.com>
2021-03-07 19:25:47 +0300

    feat: piano key active on click

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -4,10 +4,16 @@ function playAudio(audio) {
     audio.play();
 }
 
-document.getElementById('piano').onclick = function (event) {
+document.getElementById('piano').addEventListener( 'mousedown', function (event) {
     let note = event.target.dataset.note;
     if (note) {
         event.target.classList.add("piano-key-active");
         playAudio(document.querySelector(`audio[data-note="${note}"]`));
     }
-};
+});
+
+document.getElementById('piano').addEventListener( 'mouseup', function (event) {
+
+        event.target.classList.remove("piano-key-active");
+
+});
