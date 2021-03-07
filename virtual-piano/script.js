4c9ad2576f82ba531b36636a884a1784085800e0
elenabarashkova <lena.barashkova23@gmail.com>
2021-03-07 19:21:45 +0300

    fix: simplify js

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -4,17 +4,10 @@ function playAudio(audio) {
     audio.play();
 }
 
-class Piano {
-    constructor(elem) {
-        this._elem = elem;
-        elem.onclick = this.onClick.bind(this); // (*)
+document.getElementById('piano').onclick = function (event) {
+    let note = event.target.dataset.note;
+    if (note) {
+        event.target.classList.add("piano-key-active");
+        playAudio(document.querySelector(`audio[data-note="${note}"]`));
     }
-
-    onClick(event) {
-        let note = event.target.dataset.note;
-        if (note) {
-            playAudio(document.querySelector(`audio[data-note="${note}"]`));
-        }
-    };
-}
-new Piano(document.getElementById('piano'));
+};
