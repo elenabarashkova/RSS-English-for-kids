afc63be990117c718daf010e592fd14db6a7b6fd
elenabarashkova <lena.barashkova23@gmail.com>
2021-03-09 21:38:21 +0300

    feat: piano plays on letters click

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -17,3 +17,19 @@ document.getElementById('piano').addEventListener( 'mouseup', function (event) {
         event.target.classList.remove("piano-key-active");
 
 });
+
+document.addEventListener('keydown', function(event) {
+    let pushCode = event.code[3];
+    let pushPianoKey = document.querySelector(`.piano-key[data-letter="${pushCode}"]`);
+    if(pushPianoKey) {
+        pushPianoKey.classList.add("piano-key-active");
+        let note = pushPianoKey.dataset.note;
+        playAudio(document.querySelector(`audio[data-note="${note}"]`));
+    }
+});
+
+document.addEventListener('keyup', function(event) {
+    document.querySelector(`.piano-key[data-letter="${event.code[3]}"]`).classList.remove("piano-key-active");
+
+});
+
