6bd0cb4ec6a19f2a55a1626c24b6b18211bfb09e
elenabarashkova <lena.barashkova23@gmail.com>
2021-03-12 16:24:31 +0300

    feat: fullscreen available

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -84,3 +84,38 @@ tabBtnWrap.onclick = function(event) {
     }
 };
 
+// fullscreen
+
+function activateFullscreen(element) {
+    if(element.requestFullscreen) {
+        element.requestFullscreen();        // W3C spec
+    }
+    else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();     // Firefox
+    }
+    else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();  // Safari
+    }
+    else if(element.msRequestFullscreen) {
+        element.msRequestFullscreen();      // IE/Edge
+    }
+};
+function deactivateFullscreen() {
+    if(document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    }
+};
+
+
+document.querySelector('.openfullscreen').onclick = function() {
+    if(document.fullscreenElement === null) {
+        activateFullscreen(document.documentElement);
+    } else {
+        deactivateFullscreen();
+    }
+};
+
