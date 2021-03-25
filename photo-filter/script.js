8fe4734f619186ec270f7ccd6e4ec25966a9add3
elenabarashkova <lena.barashkova23@gmail.com>
2021-03-25 20:59:42 +0300

    feat: add filter value for picture

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,9 +1,34 @@
 // Filters
 
+let root = document.querySelector(':root');
+function changeFilterValue(target, value) {
+  let filterName = target.getAttribute('name'); //exp.output: blur
+  let units = '';
+  switch(filterName) {
+    case 'blur':
+      units = 'px';
+      break;
+    case 'invert':
+      units = '%';
+      break;
+    case 'sepia':
+      units = '%';
+      break;
+    case 'saturate':
+      units = '%';
+      break;
+    case 'hue':
+      units = 'deg';
+      break;
+  }
+  root.style.setProperty('--'+filterName, value+units);
+}
+
 let filtersWrap = document.querySelector('.filters');
 filtersWrap.addEventListener('input', (event) => {
   let targetOutput = event.target.nextElementSibling;
   targetOutput.value = event.target.value;
+  changeFilterValue(event.target, event.target.value);
 });
 
 
