83fe77e4f9b77e71f89440da86eda9a833f7d557
elenabarashkova <lena.barashkova23@gmail.com>
2021-03-25 17:54:10 +0300

    feat: input range events work on drag, not only on click

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,12 +1,11 @@
 // Filters
 
 let filtersWrap = document.querySelector('.filters');
-filtersWrap.addEventListener('change', (event) => {
+filtersWrap.addEventListener('input', (event) => {
   let targetOutput = event.target.nextElementSibling;
   targetOutput.value = event.target.value;
 });
 
 
 
-//todo: output заполнение на mouseover а не на click
 //todo: на клик и mouseover визуальные эффекты у range и output
