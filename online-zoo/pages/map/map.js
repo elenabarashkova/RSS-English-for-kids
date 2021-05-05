aaf83bdd1add0d636966c7503f8fe76363bd4c94
elenabarashkova <lena.barashkova23@gmail.com>
2021-05-05 18:51:19 +0300

    feat: map slider - active map item on click on slide

--- online-zoo/pages/map/map.js
+++ online-zoo/pages/map/map.js
@@ -2,17 +2,39 @@ window.addEventListener('load', function() {
   class MapSlider extends Slider {
     constructor(params) {
       super(params);
-      this.mapElems = document.querySelector(params.mapIcons);
+      this.mapElems = document.getElementsByClassName(params.mapIcons);
+      //call
+      for(let i = 0; i < this.slides.length; i ++) {
+        this.slides[i].addEventListener('click', this.onSlideClickMap);
+      }
     };
+    addActive(index) {
+      for(let i = 0; i < this.mapElems.length; i++) {
+        this.mapElems[i].classList.remove('active');
+      }
+      super.addActive(index);
+      let sliderItemDataName = this.slides[this.activeIndex].dataset.name;
+      let mapPin;
+
+      for(let i = 0; i < this.mapElems.length; i++) {
+        if(this.mapElems[i].dataset.name ===  sliderItemDataName) {
+          mapPin = this.mapElems[i];
+        }
+      }
+      if(mapPin) {
+        mapPin.classList.add('active');
+      }
+    }
   }
 
   const mapParams = {
     element: document.querySelector('.map-slider'),
     activeIndex: 1,
     slidersShownNum: window.matchMedia("(min-width: 1920px)").matches ? 8 : 5,
+    activeOnClick: true,
     slides: 'aside-slide',
     sliderInner: '.aside-slider-inner',
-    mapIcons: '.map-animal-icon',
+    mapIcons: 'map-animal-icon',
   };
   const mapSlider = window.mapSlider = new MapSlider(mapParams);
 });
