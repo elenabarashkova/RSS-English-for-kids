610be16365d53da284616b83774aefc5999f304b
elenabarashkova <lena.barashkova23@gmail.com>
2021-05-05 23:37:28 +0300

    feat: js for zoos page

--- online-zoo/pages/zoos/zoos.js
+++ online-zoo/pages/zoos/zoos.js
@@ -1,3 +1,19 @@
-/**
- * Created by Lena on 30.03.21.
- */
+window.addEventListener('load', function() {
+  let mainVideoIframe = document.querySelector('.broadcast-main iframe');
+  const smallVideos = document.getElementsByClassName('broadcast-slide');
+  let clickedVideo;
+  for(let i = 0; i < smallVideos.length; i++) {
+    smallVideos[i].addEventListener('click', onVideoClick);
+  }
+  function onVideoClick(event) {
+    clickedVideo = event.currentTarget;
+    //debugger;
+    //console.log('event.currentTarget '+event.currentTarget);
+    mainVideoIframe = document.querySelector('.broadcast-main iframe');
+    let smallVideoIframe = clickedVideo.querySelector('.broadcast-slide-inner iframe');
+    //console.log('smallVideoIframe '+smallVideoIframe);
+    let swap = mainVideoIframe.src.replace('?autoplay=1', '');
+    mainVideoIframe.src = smallVideoIframe.src + '?autoplay=1';
+    smallVideoIframe.src = swap;
+  }
+});
