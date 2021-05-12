ed646d60c18f53c9f6312e727bb72720b5f5b866
elenabarashkova <lena.barashkova23@gmail.com>
2021-05-12 21:19:49 +0300

    feat: register form + validation

--- match-match-game/src/pages/register/register-form.ts
+++ match-match-game/src/pages/register/register-form.ts
@@ -1,8 +1,16 @@
 import { openPopup } from "../../popup";
 import { insertHtml } from "../../shared";
+import "./validation";
 
 const registerPopupHtml = require('./register-form.html');
 
 insertHtml(registerPopupHtml.default);
 const registerBtn = document.getElementById('registerBtn');
 registerBtn?.addEventListener('click', openPopup);
+
+const registerForm = document.getElementById('registerForm');
+
+// On cancel click
+document.getElementById('cancel')?.addEventListener('cancel', () => {
+  (registerForm as HTMLFormElement)?.reset();
+});
---
+++ match-match-game/src/pages/register/validation.ts
@@ -0,0 +1,55 @@
+const registerSubmit = document.getElementById('registerSubmit');
+const registerFields: HTMLCollection = document.getElementsByClassName('register-input');
+
+function valid(field: HTMLInputElement) {
+  field.classList.remove('invalid');
+  field.classList.add('valid');
+  const errorItem = field.parentElement?.querySelector('.error-text') as HTMLElement;
+  if(errorItem) {
+    errorItem.innerText = '';
+  }
+}
+
+function invalid(field: HTMLInputElement) {
+  field.classList.remove('valid');
+  field.classList.add('invalid');
+  if(field?.parentElement?.querySelector('.error-text')) {
+    return field.nextSibling as HTMLElement;
+  }
+  const errorItem = document.createElement('span');
+  errorItem.classList.add('error-text');
+  field.insertAdjacentElement('afterend', errorItem);
+  return errorItem;
+}
+
+function inputValidation(currentField: HTMLInputElement) {
+  if(currentField.validity.patternMismatch || currentField.validity.typeMismatch) {
+    invalid(currentField).innerText = 'Invalid value. Please correct your data.';
+    return false;
+  }
+  if(!currentField.value) {
+    invalid(currentField).innerText = 'Empty value. Please fill in the field.';
+    return false;
+  }
+  valid(currentField);
+  return true;
+}
+
+function onInputChange(event: Event) {
+  inputValidation(event.target as HTMLInputElement);
+}
+
+function onSubmit(event: Event) {
+  event.preventDefault();
+  let isValid = true;
+  for(let i = 0; i < registerFields.length; i++) {
+    isValid = inputValidation(registerFields[i] as HTMLInputElement) && isValid;
+  }
+  // todo save data if isValid
+}
+
+for(let i = 0; i < registerFields.length; i++) {
+  (registerFields[i] as HTMLInputElement).addEventListener('change', onInputChange)
+}
+
+registerSubmit?.addEventListener('click', onSubmit);
