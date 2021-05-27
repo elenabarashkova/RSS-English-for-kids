2eed80ae6fcd103f0390b83095d15bc78925fad5
elenabarashkova <lena.barashkova23@gmail.com>
2021-05-27 23:41:02 +0300

    refactor: use reduce to shorten code

--- match-match-game/src/pages/register/addUser.ts
+++ match-match-game/src/pages/register/addUser.ts
@@ -3,16 +3,14 @@ import { addUser } from "../../common/indexedDB";
 import {getCurrentPhoto} from "./photo-behavior";
 
 export const saveFormData = ():void => {
-  const formItemsElements = {
-    firstNameInput: document.getElementById('firstName'),
-    lastNameInput: document.getElementById('lastName'),
-    emailInput: document.getElementById('email'),
-  };
-  const personData: PersonData = {
-    firstName: (formItemsElements.firstNameInput as HTMLInputElement)?.value,
-    lastName: (formItemsElements.lastNameInput as HTMLInputElement)?.value,
-    email: (formItemsElements.emailInput as HTMLInputElement)?.value,
-    userPhoto: getCurrentPhoto(),
-  };
+
+  const personData: PersonData = ['firstName', 'lastName', 'email'].reduce(
+    (acc, fieldName) => ({
+      ...acc,
+      [fieldName]: (document.getElementById(fieldName) as HTMLInputElement).value,
+    }),
+    { userPhoto: getCurrentPhoto() }
+  );
+
   addUser(personData);
 }
