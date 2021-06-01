c61b20e8a722b6662b6f1f3cee474ab7e961997b
elenabarashkova <lena.barashkova23@gmail.com>
2021-06-01 12:20:36 +0300

    refactor: add types for action types storage

--- async-race/src/redux/actions.ts
+++ async-race/src/redux/actions.ts
@@ -1,4 +1,6 @@
+import { SET_ACTIVE_PAGE } from "./types";
+
 export const setActivePage = (page: string) => ({
-  type: 'SET_ACTIVE_PAGE',
+  type: SET_ACTIVE_PAGE,
   payload: page,
 })
--- async-race/src/redux/root-reducer.ts
+++ async-race/src/redux/root-reducer.ts
@@ -1,5 +1,7 @@
+import { SET_ACTIVE_PAGE } from "./types";
+
 export const rootReducer = (state:Record<string, string>, action:Record<string, string>) => {
-  if(action.type === 'SET_ACTIVE_PAGE') {
+  if(action.type === SET_ACTIVE_PAGE) {
     return {
       ...state,
       currentPage: action.payload,
