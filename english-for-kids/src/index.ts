f3c94c710e9028b4c9b1387346ada2c1a6ed2d4b
elenabarashkova <lena.barashkova23@gmail.com>
2021-06-24 15:16:12 +0300

    feat: styles for toggler of game mode

--- english-for-kids/src/index.ts
+++ english-for-kids/src/index.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 import store from "./redux/store";
-// import { toggleGameMode } from "./redux/actions";
+import { toggleGameMode } from "./redux/actions";
 import { initCommonPageTemplate } from "./components";
 import { startRouter } from "./router";
 
@@ -11,8 +11,7 @@ window.addEventListener('load', () => {
 
   startRouter();
 
-  // document.getElementById('checkbox')?.addEventListener('click', () => {
-  //   toggleGameMode();
-  //   console.log(store.getState());
-  // })
+  document.getElementById('checkbox')?.addEventListener('click', () => {
+    toggleGameMode();
+  })
 })
