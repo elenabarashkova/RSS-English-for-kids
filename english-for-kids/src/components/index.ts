620131e835ed96cc1253ccb4a081882c406c41f5
elenabarashkova <lena.barashkova23@gmail.com>
2021-06-28 22:25:04 +0300

    feat: footer styles

--- english-for-kids/src/components/index.ts
+++ english-for-kids/src/components/index.ts
@@ -10,14 +10,25 @@ const getMain = (): string => (`
   </main>
 `)
 
+const githubIcon = require('../assets/github.svg');
+const rssIcon = require('../assets/rss.svg');
+
 const getFooter = (): string => (`
   <footer id="footer" class="footer">
     <div class="container">
-      <a class="github" href="https://github.com/elenabarashkova" target="_blank">elenabarashkova</a>
-      <div>2021</div>
-      <a class="rss" href="https://rs.school/js/" target="_blank" rel="noopener noreferrer">
-        <span class="rss-year">'21</span>
-      </a>
+      <div class="footer-item">
+        ${githubIcon}
+        <a class="github" href="https://github.com/elenabarashkova" target="_blank">elenabarashkova</a>
+      </div>
+      <div class="footer-item">
+        <span class="footer-year">2021</span>
+      </div>
+      <div class="footer-item">
+        ${rssIcon}
+        <a class="rss" href="https://rs.school/js/" target="_blank">
+          <span class="rss-year">'21</span>
+        </a>
+      </div>
     </div>
   </footer>
 `)
