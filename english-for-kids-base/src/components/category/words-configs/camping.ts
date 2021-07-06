10270c9da47a88ae8d87661344c1ea9ac1da4b81
elenabarashkova <lena.barashkova23@gmail.com>
2021-07-06 17:30:32 +0300

    feat: add content

---
+++ english-for-kids-base/src/components/category/words-configs/camping.ts
@@ -0,0 +1,60 @@
+import { getImagePath, getSoundPath } from "./index";
+
+export const wordsListConfigCamping: WordsListConfig = [
+  {
+    word: 'tent',
+    name: 'Tent',
+    translation: 'Палатка',
+    imageUrl: getImagePath('camping','tent'),
+    sound: getSoundPath('camping','tent')
+  },
+  {
+    word: 'backpack',
+    name: 'Backpack',
+    translation: 'Рюкзак',
+    imageUrl: getImagePath('camping','backpack'),
+    sound: getSoundPath('camping','backpack')
+  },
+  {
+    word: 'kayak',
+    name: 'Kayak',
+    translation: 'Байдарка',
+    imageUrl: getImagePath('camping','kayak'),
+    sound: getSoundPath('camping','kayak')
+  },
+  {
+    word: 'map',
+    name: 'Map',
+    translation: 'Карта',
+    imageUrl: getImagePath('camping','map'),
+    sound: getSoundPath('camping','map')
+  },
+  {
+    word: 'sleepingbag',
+    name: 'Sleeping bag',
+    translation: 'Спальник',
+    imageUrl: getImagePath('camping','sleepingbag'),
+    sound: getSoundPath('camping','sleepingbag')
+  },
+  {
+    word: 'woods',
+    name: 'Woods',
+    translation: 'Лес',
+    imageUrl: getImagePath('camping','woods'),
+    sound: getSoundPath('camping','woods')
+  },
+  {
+    word: 'campfire',
+    name: 'Campfire',
+    translation: 'Костер',
+    imageUrl: getImagePath('camping','campfire'),
+    sound: getSoundPath('camping','campfire')
+  },
+  {
+    word: 'flashlight',
+    name: 'Flashlight',
+    translation: 'Фонарик',
+    imageUrl: getImagePath('camping','flashlight'),
+    sound: getSoundPath('camping','flashlight')
+  },
+]
--- english-for-kids-base/src/components/main-page/categories-config.ts
+++ english-for-kids-base/src/components/main-page/categories-config.ts
@@ -2,6 +2,12 @@ import { CATEGORIES_LIST } from "./constants";
 import { wordsListConfigAnimals } from "../category/words-configs/animals";
 import { wordsListConfigTransport } from "../category/words-configs/transport";
 import { getCategoryImagePath } from "../category/words-configs"
+import { wordsListConfigEcology } from "../category/words-configs/ecology";
+import { wordsListConfigCamping } from "../category/words-configs/camping";
+import { wordsListConfigFruits } from "../category/words-configs/fruits";
+import { wordsListConfigFood } from "../category/words-configs/food";
+import { wordsListConfigHouse } from "../category/words-configs/house";
+import { wordsListConfigFarm } from "../category/words-configs/farm";
 
 export const categoriesListConfig: CategoriesListConfig = {
   [CATEGORIES_LIST.ANIMALS]: {
@@ -19,37 +25,37 @@ export const categoriesListConfig: CategoriesListConfig = {
   [CATEGORIES_LIST.ECOLOGY]: {
     name: 'Ecology',
     url: 'ecology',
-    imageUrl: getCategoryImagePath('animals'),
-    wordsConfig: wordsListConfigAnimals,
+    imageUrl: getCategoryImagePath('ecology'),
+    wordsConfig: wordsListConfigEcology,
   },
   [CATEGORIES_LIST.CAMPING]: {
     name: 'Camping',
     url: 'camping',
-    imageUrl: getCategoryImagePath('animals'),
-    wordsConfig: wordsListConfigAnimals,
+    imageUrl: getCategoryImagePath('camping'),
+    wordsConfig: wordsListConfigCamping,
   },
   [CATEGORIES_LIST.FRUITS]: {
     name: 'Fruits',
     url: 'fruits',
-    imageUrl: getCategoryImagePath('animals'),
-    wordsConfig: wordsListConfigAnimals,
+    imageUrl: getCategoryImagePath('fruits'),
+    wordsConfig: wordsListConfigFruits,
   },
   [CATEGORIES_LIST.FOOD]: {
     name: 'Food',
     url: 'food',
-    imageUrl: getCategoryImagePath('animals'),
-    wordsConfig: wordsListConfigAnimals,
+    imageUrl: getCategoryImagePath('food'),
+    wordsConfig: wordsListConfigFood,
   },
   [CATEGORIES_LIST.HOUSE]: {
     name: 'House',
     url: 'house',
-    imageUrl: getCategoryImagePath('animals'),
-    wordsConfig: wordsListConfigAnimals,
+    imageUrl: getCategoryImagePath('house'),
+    wordsConfig: wordsListConfigHouse,
   },
   [CATEGORIES_LIST.FARM]: {
     name: 'Farm',
     url: 'farm',
-    imageUrl: getCategoryImagePath('animals'),
-    wordsConfig: wordsListConfigAnimals,
+    imageUrl: getCategoryImagePath('farm'),
+    wordsConfig: wordsListConfigFarm,
   },
 }
