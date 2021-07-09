c7d05f80b4daa29112c4f01ba312f7f9ee0c26bc
elenabarashkova <lena.barashkova23@gmail.com>
2021-07-09 15:19:28 +0300

    feat: header in admin-panel

--- english-for-kids-base/src/components/admin-panel/render.ts
+++ english-for-kids-base/src/components/admin-panel/render.ts
@@ -1,6 +1,20 @@
+import { ADMIN_ROUTE } from "../../router/constants";
+
 export const getAdminPanelInner = (): string => (`
   <div id="AdminPanelPage" class="admin-panel-page">
+    <nav class="admin-navigation">
+      <div class="container">
+        <ul class="admin-menu">
+          <li class="admin-menu-item active">
+            <a href="#${ADMIN_ROUTE}">Categories</a>
+          </li>
+          <li class="admin-menu-item">
+            <span>Words</span>
+          </li>
+        </ul>
+        <button id="logoutBtn" class="login-btn">Logout</button>
+      </div>
+    </nav>
     <h2>AdminPanel</h2>
-    <button id="logoutBtn" class="login-btn">Logout</button>
   </div>
 `)
